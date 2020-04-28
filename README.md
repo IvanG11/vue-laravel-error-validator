bce3dcc8a09b9c77579186fe176938ae0709ebdb
Iván García <ivangb091@gmail.com>
2020-04-28 16:48:39 +0200

    update

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vue Laravel Errors
+
+This package allow to display errors from laravel
+
+# Installation
+
+You can install the package via npm:
+
+```
+npm install --save vue-laravel-error-validator
+```
+
+Import the package:
+
+```javascript
+import VueLaravelErrorValidator from "vue-laravel-error-validator";
+
+Vue.use(VueLaravelErrorValidator);
+```
--- src/Errors.js
+++ src/Errors.js
@@ -4,38 +4,88 @@ class Errors {
         this.errors = {};
     }
 
-    has(key){
-        return this.errors[key] !== undefined
+    /**
+     * Determine if any errors exists for the given field or object.
+     *
+     * @param {string|null} field
+     */
+    has(field){
+        return this.errors[field] !== undefined
     }
 
+    /**
+     * Return the first error for the given field or object.
+     *
+     * @param {string|null} field
+     */
     first(key) {
         if(this.has(key)){
             return this.errors[key][0]
         }
     }
 
-    get(key) {
-        if(this.has(key)){
-            return this.errors[key]
-        }
+    /**
+     * Determine if we have any errors.
+     */
+    any() {
+        return Object.keys(this.errors).length > 0;
     }
 
+    /**
+     * Get field that error
+     * @param field
+     * @returns {*|*[]}
+     */
+    get(field) {
+        return this.errors[field] || [];
+    }
+
+    /**
+     * Get all errors
+     * @returns {{}}
+     */
     all() {
         return this.errors
     }
 
-    record(values) {
-        this.errors = values
+    /**
+     * Flush error
+     */
+    flush() {
+        this.errors = {};
     }
 
+    /**
+     * Record the error object
+     * @param errors
+     */
+    record(errors = {}) {
+        this.errors = errors;
+    }
+
+    /**
+     * Clear one or all error fields.
+     *
+     * @param {String|undefined} field
+     */
     clear(field) {
-        if (field) {
-            delete this.errors[field];
+        if (!field) return this.flush();
+        let errors = Object.assign({}, this.errors);
+        Object.keys(errors)
+            .filter(e => e === field || e.startsWith(`${field}.`) || e.startsWith(`${field}[`))
+            .forEach(e => delete errors[e]);
+        this.fill(errors);
+    }
 
-            return;
+    /**
+     * Clear errors on keydown.
+     *
+     * @param {KeyboardEvent} event
+     */
+    onKeydown(event) {
+        if (event.target.name) {
+            this.clear(event.target.name);
         }
-
-        this.errors = {};
     }
 
 }
